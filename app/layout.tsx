a49394e833918071f9aedeba8789cfb63102ecb3
Sourav <palsourav9112@gmail.com>
2026-05-20 16:42:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,12 @@ import { Toaster } from "react-hot-toast";
 import { cn } from "@/lib/utils";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
 
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={cn("font-sans")}>
       <body className="font-poppins antialiased">
-       <ClerkProvider>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">{children}</main>
@@ -25,7 +23,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             },
           }}
         />
-        </ClerkProvider>
       </body>
     </html>
   );
