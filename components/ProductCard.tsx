be82f07b80994c7e13f701f11e8e5b1866505ed6
Sourav <palsourav9112@gmail.com>
2026-06-06 17:47:19 +0530

    Refactor ProductCard component structure and imports

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -14,8 +14,8 @@ const ProductCard = ({ product }: { product: Product }) => {
   return (
     <div className="text-sm border-[1px] rounded-md border-darkBlue/20 group bg-white">
       <div className="relative group overflow-hidden bg-shop_light_bg">
-        {product?.images && (
-          <Link href={`/product/${product?.slug?.current}`}>
+        {product.images && (
+          <Link href={`/product/${product.slug.current}`}>
             <Image
               src={urlFor(product.images[0]).url()}
               alt="productImage"
@@ -23,12 +23,12 @@ const ProductCard = ({ product }: { product: Product }) => {
               height={500}
               priority
               className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg duration-500 
-              ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
+              ${product.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
             />
           </Link>
         )}
         <ProductSideMenu product={product} />
-        {product?.status === "sale" ? (
+        {product.status === "sale" ? (
           <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-lightGreen hover:text-shop_dark_green hoverEffect">
             Sale!
           </p>
@@ -46,12 +46,12 @@ const ProductCard = ({ product }: { product: Product }) => {
         )}
       </div>
       <div className="p-3 flex flex-col gap-2">
-        {product?.categories && (
+        {product.categories && (
           <p className="uppercase line-clamp-1 text-xs font-medium text-lightText">
             {product.categories.map((cat) => cat).join(", ")}
           </p>
         )}
-        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+        <Title className="text-sm line-clamp-1">{product.name}</Title>
         <div className="flex items-center gap-2">
           <div className="flex items-center">
             {[...Array(5)].map((_, index) => (
@@ -70,15 +70,15 @@ const ProductCard = ({ product }: { product: Product }) => {
         <div className="flex items-center gap-2.5">
           <p className="font-medium">In Stock</p>
           <p
-            className={`${product?.stock === 0 ? "text-red-600" : "text-shop_dark_green/80 font-semibold"}`}
+            className={`${product.stock === 0 ? "text-red-600" : "text-shop_dark_green/80 font-semibold"}`}
           >
-            {(product?.stock as number) > 0 ? product?.stock : "unavailable"}
+            {(product.stock as number) > 0 ? product.stock : "unavailable"}
           </p>
         </div>
 
         <PriceView
-          price={product?.price}
-          discount={product?.discount}
+          price={product.price}
+          discount={product.discount}
           className="text-sm"
         />
         <AddToCartButton product={product} className="w-36 rounded-full" />
